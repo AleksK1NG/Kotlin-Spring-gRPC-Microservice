00e77655b55e24591f7f813e0c2beabda76c1502
Alexander <alexander.bryksin@yandex.ru>
2022-10-16 15:09:23 +0300

    feature: add logging interceptor

---
+++ src/main/kotlin/com/example/alexbryksin/interceptors/GlobalInterceptorConfiguration.kt
@@ -0,0 +1,14 @@
+package com.example.alexbryksin.interceptors
+
+import net.devh.boot.grpc.server.interceptor.GrpcGlobalServerInterceptor
+import org.springframework.context.annotation.Configuration
+
+
+@Configuration(proxyBeanMethods = false)
+class GlobalInterceptorConfiguration {
+
+    @GrpcGlobalServerInterceptor
+    fun logServerInterceptor(): LogGrpcInterceptor? {
+        return LogGrpcInterceptor()
+    }
+}
---
+++ src/main/kotlin/com/example/alexbryksin/interceptors/LogGrpcInterceptor.kt
@@ -0,0 +1,20 @@
+package com.example.alexbryksin.interceptors
+
+import com.example.alexbryksin.delivery.grpc.BankAccountGrpcService
+import io.grpc.Metadata
+import io.grpc.ServerCall
+import io.grpc.ServerCallHandler
+import io.grpc.ServerInterceptor
+import org.slf4j.LoggerFactory
+
+class LogGrpcInterceptor : ServerInterceptor {
+
+    override fun <ReqT : Any?, RespT : Any?> interceptCall(call: ServerCall<ReqT, RespT>, headers: Metadata, next: ServerCallHandler<ReqT, RespT>): ServerCall.Listener<ReqT> {
+        log.info("Service: ${call.methodDescriptor.serviceName}, Method: ${call.methodDescriptor.bareMethodName}, headers: $headers")
+        return next.startCall(call, headers)
+    }
+
+    companion object {
+        private val log = LoggerFactory.getLogger(BankAccountGrpcService::class.java)
+    }
+}
