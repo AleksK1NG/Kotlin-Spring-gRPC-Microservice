c10601c3aa5417291a422d6e79cb556230327bb6
Alexander Bryksin <bryksin_as@itms.tech>
2022-10-17 14:13:48 +0300

    feature: add postgres repository and flyway migrations

---
+++ Makefile
@@ -0,0 +1,29 @@
+.PHONY:
+
+# ==============================================================================
+# Docker
+
+local:
+	@echo Starting local docker compose
+	docker-compose -f docker-compose.local.yaml up -d --build
+
+develop:
+	mvn clean package -Dmaven.test.skip
+	@echo Starting docker compose
+	docker-compose -f docker-compose.yaml up -d --build
+
+
+# ==============================================================================
+# Docker and k8s support grafana - prom-operator
+
+FILES := $(shell docker ps -aq)
+
+down-local:
+	docker stop $(FILES)
+	docker rm $(FILES)
+
+clean:
+	docker system prune -f
+
+logs-local:
+	docker logs -f $(FILES)
---
+++ src/main/resources/db/migration/V1__initial_setup.sql
@@ -0,0 +1,17 @@
+CREATE EXTENSION IF NOT EXISTS citext;
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+CREATE SCHEMA microservices;
+
+
+CREATE TABLE IF NOT EXISTS microservices.bank_accounts
+(
+    bank_account_id UUID                         DEFAULT uuid_generate_v4(),
+    email           VARCHAR(250) UNIQUE NOT NULL CHECK ( email <> '' ),
+    phone           VARCHAR(60) UNIQUE  NOT NULL CHECK ( phone <> '' ),
+    balance         DECIMAL(16, 2)      NOT NULL DEFAULT 0.00,
+    currency        VARCHAR(3)          NOT NULL DEFAULT 'USD',
+    created_at      TIMESTAMP WITH TIME ZONE     DEFAULT CURRENT_TIMESTAMP,
+    updated_at      TIMESTAMP WITH TIME ZONE     DEFAULT CURRENT_TIMESTAMP
+                                  );
+
+CREATE INDEX IF NOT EXISTS bank_account_email_idx ON microservices.bank_accounts (email);
