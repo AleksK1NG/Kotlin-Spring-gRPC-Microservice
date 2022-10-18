72a9ec35b6a260bd79f22ca646ecd9c8323f9c70
Alexander Bryksin <bryksin_as@itms.tech>
2022-10-18 14:48:41 +0300

    feature: add data loader improvements

--- src/main/kotlin/com/example/alexbryksin/configuration/DataLoaderConfig.kt
+++ src/main/kotlin/com/example/alexbryksin/configuration/DataLoaderConfig.kt
@@ -4,8 +4,10 @@ import com.example.alexbryksin.domain.BankAccount
 import com.example.alexbryksin.domain.Currency
 import com.example.alexbryksin.service.BankAccountService
 import com.github.javafaker.Faker
+import kotlinx.coroutines.async
 import kotlinx.coroutines.runBlocking
 import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.context.annotation.Configuration
@@ -20,26 +22,30 @@ class DataLoaderConfig(
     private val faker: Faker,
 ) : CommandLineRunner {
 
+    @Value(value = "\${faker.count:300}")
+    val count: Int = 300
+
     override fun run(vararg args: String?) = runBlocking {
 
         try {
-            (0..100).forEach { _ ->
-                val createdBankAccount = bankAccountService.createBankAccount(
-                    BankAccount(
-                        id = null,
-                        email = faker.internet().emailAddress(),
-                        firstName = faker.name().firstName(),
-                        lastName = faker.name().lastName(),
-                        address = faker.address().fullAddress(),
-                        phone = faker.phoneNumber().cellPhone(),
-                        currency = Currency.USD,
-                        balance = BigDecimal.valueOf(faker.number().numberBetween(0, 500000).toDouble()),
-                        createdAt = LocalDateTime.now(),
-                        updatedAt = LocalDateTime.now()
+            (0..count).map { _ ->
+                async {
+                    bankAccountService.createBankAccount(
+                        BankAccount(
+                            id = null,
+                            email = faker.internet().emailAddress(),
+                            firstName = faker.name().firstName(),
+                            lastName = faker.name().lastName(),
+                            address = faker.address().fullAddress(),
+                            phone = faker.phoneNumber().cellPhone(),
+                            currency = Currency.USD,
+                            balance = BigDecimal.valueOf(faker.number().numberBetween(0, 500000).toDouble()),
+                            createdAt = LocalDateTime.now(),
+                            updatedAt = LocalDateTime.now()
+                        )
                     )
-                )
-                log.info("created bank account: $createdBankAccount")
-            }
+                }
+            }.map { it.await() }.forEach { log.info("created bank account: $it") }
             log.info("Mock data successfully inserted")
         } catch (ex: Exception) {
             log.error("insert mock data error", ex)
